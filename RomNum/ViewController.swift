13d2116f520813145b40b72732da8b1d79d8749c
Nasser Eledroos <nasser@eledroos.net>
2016-05-19 10:27:47 -0400

    Added Grid Numbers with Constraints, appending to UILabel and Int value

--- RomNum/ViewController.swift
+++ RomNum/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var topLabel: UILabel!
+    var labelVal: Int! = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        topLabel.text = ""
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,88 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func oneButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "1"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+    @IBAction func twoButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "2"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+    @IBAction func threeButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "3"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+    @IBAction func fourButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "4"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+    @IBAction func fiveButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "5"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
 
+    @IBAction func sixButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "6"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+    @IBAction func sevenButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "7"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+    @IBAction func eightButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "8"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+    @IBAction func nineButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "9"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+    @IBAction func zeroButtonPressed(sender: AnyObject) {
+        if let txt = topLabel?.text {
+            topLabel?.text = txt + "0"
+            labelVal = Int(topLabel.text!)
+            print(labelVal)
+        }
+    }
+    
+
+    
 }
 
